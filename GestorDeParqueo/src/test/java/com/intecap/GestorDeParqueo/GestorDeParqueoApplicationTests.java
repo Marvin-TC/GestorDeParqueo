c2248bf6bd75cbb3c8db47bc94b0fba67ba95e78
Marvin-TC <51245868+Marvin-TC@users.noreply.github.com>
2025-04-05 14:54:16 -0600

    Initial commit

---
+++ GestorDeParqueo/src/test/java/com/intecap/GestorDeParqueo/GestorDeParqueoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intecap.GestorDeParqueo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeParqueoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
